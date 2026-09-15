cdf2b0c0027d198f5d960ddc81eaadd542404385
Fazli <alghanifazli@gmail.com>
2026-09-15 15:05:11 -0400

    LL Full Class

---
+++ Linked List/LinkedListFullClass.py
@@ -0,0 +1,40 @@
+### CREDIT: Anthony Sistilli (2018)
+
+class linkedListNode:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.nextNode = nextNode
+
+class linkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, value):
+        node = linkedListNode(value)
+        if self.head is None:
+            self.head = node
+            return
+
+        currentNode = self.head
+        while True:
+            if currentNode.nextNode is None:
+                currentNode.nextNode = node
+                break
+            currentNode = currentNode.nextNode
+
+    def printLinkedList(self):
+        currentNode = self.head
+        while currentNode is not None:
+            print(currentNode.value, "->", end=" ")
+            currentNode = currentNode.nextNode
+        print("None")
+
+# TEST
+ll = linkedList()
+ll.printLinkedList()
+ll.insert("3")
+ll.printLinkedList()
+ll.insert("5")
+ll.printLinkedList()
+ll.insert("7")
+ll.printLinkedList()
