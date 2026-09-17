7f8655246e5b331be9c4d8a76f86f9314fe1d699
Fazli <alghanifazli@gmail.com>
2026-09-17 00:36:01 -0400

    STRING Fundamental

--- String/string.py
+++ String/string.py
@@ -1,3 +1,5 @@
+###CREDIT: W3SCHOOLS
+
 # PRINT
 print("Hello")
 print('Hello')
@@ -7,3 +9,37 @@ print('It\'s')
 
 a = "Hello"
 print(a)
+
+#Multiline Strings
+b = """Lorem lorem
+ipsum ipsum
+dolor dolor"""
+print(b)
+
+b = '''Lorem lorem
+ipsum ipsum
+dolor dolor'''
+print(b)
+
+###NOTE: Strings are Arrays in Python. There is no char data type in Python.
+a = "Hello World!"
+print(a[1])
+
+#Looping Through a String
+for x in "banana":
+    print(x)
+
+#String Length
+a = "Hello World!"
+print(len(a))
+
+#Check String
+txt = "The best freestyle"
+print("free" in txt)
+
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+#Check if NOT
+txt = "The best things in life are free!"
+print("expensive" not in txt)
